ddeb1bb8a029dd101b9c941f6321166832fa9eba
dexter <32395272+turnopil@users.noreply.github.com>
2017-10-18 20:57:45 +0300

    main changes

--- Weather/js/main.js
+++ Weather/js/main.js
@@ -4,18 +4,40 @@
     var val = document.getElementsByTagName("option")[opt].value
     alert(val);
 }*/
-function token(){
+
+
+	document.getElementById("city").addEventListener("change", function () {
+		
 	var API_URL = "http://api.openweathermap.org/data/2.5/weather?id=";
 	var key = "a8af1cdd0f3b549170961d8985b7b05e";
-	var opt = document.getElementById("city").selectedIndex;
-	var val = document.getElementsByTagName("option")[opt].value
-
-	if (val===0) {
+	
+	//var opt = document.getElementById("city").selectedIndex;
+	//var val = document.getElementsByTagName("option")[opt].value;
+	//console.log(val);
+					
+	if (city.value.length === 0) {
 		return;
 	} 
 	else {
+		var uri = API_URL + city.value + "&appid=" + key;
+		//console.log(uri);
 
-		var uri = API_URL + val + "&appid" + key;
-		console.log(uri) 
+	var XHR = new XMLHttpRequest(); //відкриття порталу
+		XHR.open("GET", uri, true); //true - асинхронно
+		XHR.send();// послати запрос 
+
+		XHR.onreadystatechange = function () {
+	
+	if ((XHR.readyState === 4)&&(XHR.status === 200)) {
+		console.log(XHR.responseText);
+
+
+	var data = JSON.parse(XHR.responseText);
+		main.innerHTML = "<h1>(data.main.humidity)</h1>";
+
+			}
+		}
 	}
-}
+}, false);
+
+
