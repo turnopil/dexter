5a7570716527ca0ddf4d5b36bcc8ea5ed41132e1
turnopil <32395272+turnopil@users.noreply.github.com>
2017-10-18 17:24:15 +0300

    weather main.js add

--- Weather/js/main.js
+++ Weather/js/main.js
@@ -1,4 +1,21 @@
-function(){
+/*function selectID() {
+    var opt = document.getElementById("city").selectedIndex;
+    console.log(opt)
+    var val = document.getElementsByTagName("option")[opt].value
+    alert(val);
+}*/
+function token(){
+	var API_URL = "http://api.openweathermap.org/data/2.5/weather?id=";
+	var key = "a8af1cdd0f3b549170961d8985b7b05e";
+	var opt = document.getElementById("city").selectedIndex;
+	var val = document.getElementsByTagName("option")[opt].value
 
-	var API_URL = ""
+	if (val===0) {
+		return;
+	} 
+	else {
+
+		var uri = API_URL + val + "&appid" + key;
+		console.log(uri) 
+	}
 }
